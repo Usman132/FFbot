4740fd3c5b232a8e3232b4fab658b0bd40fa8783
YouTwitFace <32808683+YouTwitFaceTG@users.noreply.github.com>
2018-07-19 15:06:53 -0400

    Delete captcha.py

--- FFbot/captcha.py
+++
@@ -1,5 +0,0 @@
-# copy this file to tg_bot/captcha.py and solve the dynamic captcha defined by the 3
-# parameters and return the expected str value Hint: You might need to think backwards to
-# find a solution
-def solve(alpha, beta, gamma):
-    return f'{beta};{alpha[::-1]};{gamma*2}'
