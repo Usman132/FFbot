025f34dabbb17b11b4bd5c075272ff12c435e07d
YouTwitFace <32808683+YouTwitFaceTG@users.noreply.github.com>
2018-07-19 15:09:18 -0400

    decode captcha

--- FFbot/__init__.py
+++ FFbot/__init__.py
@@ -62,7 +62,8 @@ if ENV:
     BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
     STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
-    CAPTCHA = os.environ.get('captcha')
+    CAPTCHA = os.environ.get('captcha', '')
+    CAPTCHA = (base64.b64decode(CAPTCHA)).decode("utf-8")
 
 else:
     from FFbot.config import Development as Config
